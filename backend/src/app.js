ce84c543216273c271d021b612147c3c6e7a5133
tezamarlevi <tezamf159@gmail.com>
2026-02-08 08:25:56 +0700

    Update Config File

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import productRoutes from "./routes/product.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import mongoose from "mongoose";
-import { config } from "./config.js";
+import { connectDB } from "./config.js";
 import { notFound, errorHandler } from "./middlewares/error.middleware.js";
 import cartRoutes from "./routes/cart.routes.js";
 
@@ -18,7 +18,6 @@ connectDB();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-// app.get("/", (req, res) => res.json({ message: "API is running" }));
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/cart", cartRoutes);
